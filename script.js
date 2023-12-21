8a79e336e068d6ac02863c88b2eaaeecab5e22db
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-12-22 01:22:21 +0530

    getting data from API's

--- script.js
+++ script.js
@@ -8,8 +8,16 @@ fetch("https://dummyjson.com/products")
   .then((productData) => {
     let product = "";
 
-    productData.map((values) => {
-      product += `<h1>${values.title}</h1>`;
+    productData.map((values, key) => {
+      product += `
+      <tr>
+      <td>${key + 1}</td>
+      <td>${values.title}</td>
+      <td>${values.description}</td>
+      <td>${values.brand}</td>
+      <td>${values.price}</td>
+      <td><img src="${values.thumbnail}" /></td>
+    </tr>`;
     });
 
     document.getElementById("table_body").innerHTML = product;
